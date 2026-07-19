f8f4de776dd7acc9cd653957ce03774d543e364e
Kieran <kieran@Mac.lan>
2026-07-19 10:27:00 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div>
+      <h1> testing testing  </h1>
+
+
+27 Labs LLC 
+
+
+      <p>Welcome to the beginning of a new world.</p>
+    </div>
+  );
+}
+
+export default App;
